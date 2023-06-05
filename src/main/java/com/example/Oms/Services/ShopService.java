b8f3234c03b4732a5ebfad263e4ce643aeda1049
Priya Premprakash Babu <premprakashb2002@gmail.com>
2023-06-05 10:17:59 +0530

    Revert "updates"
    This reverts commit e977ab5c59519b0690baa0aea46e3eb810993c6a.

--- src/main/java/com/example/Oms/Services/ShopService.java
+++ src/main/java/com/example/Oms/Services/ShopService.java
@@ -101,7 +101,6 @@ public class ShopService implements UserDetailsService {
         if (this.shopInfoRepo.existsById(shopId)) {
             ShopInfo shopInfo = this.shopInfoRepo.findById(shopId).get();
 
-
             return shopInfo;
         } else {
             response.setStatus(400);
